9b140461b6ab304d1d097a56d62099b8d85c54ea
farzanemaleki <farzane.maleki.fm@gmail.com>
2022-05-29 17:49:17 +0430

    add DataBase connection file

---
+++ includes/db.php
@@ -0,0 +1,19 @@
+<?php
+define('host_name', 'localhost');
+define('user_name', 'root');
+define('password', '');
+define('db_name', 'architect');
+
+ 
+$mysqli = new mysqli();
+$sql = $mysqli->connect(host_name, user_name, password, db_name);
+
+if($sql){
+    echo 'it works';
+}else{
+    echo 'not run';
+}
+
+
+
+?>
